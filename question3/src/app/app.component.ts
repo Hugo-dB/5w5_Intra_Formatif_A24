8f32b8b8e7a64a03dc015900b9fb95dcc9d6d995
Hugo-dB <6295691@cegepmontpetit.ca>
2026-03-11 18:21:54 -0400

    Presque finit form

--- question3/src/app/app.component.ts
+++ question3/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, FormGroup, ValidatorFn } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -18,14 +18,62 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 export class AppComponent {
   title = 'reactive.form';
   formGroup: FormGroup;
+  formData?: Data;
+
+  test() {
+      console.log(this.formGroup);
+  }
   
   constructor(private formBuilder : FormBuilder) { 
     this.formGroup = this.formBuilder.group(
       {
-        name: ['', [Validators.required]]
-      }  
+        name: ['', [Validators.required]],
+        roadnumber: ['', [Validators.required, Validators.min(1000), Validators.max(9999)]],
+        postalcode: ['', [Validators.pattern("^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$")]],
+        comments: ['', [estDixMots()]],
+      },
+      { validators: nomDansCommentaire() }
     );
+    this.formGroup.valueChanges.subscribe(v => this.formData = v)
   }
 }
+
+interface Data {
+  name?: string | null;
+  roadnumber?: string | null;
+  postalcode?: string | null;
+  comments?: string | null;
+}
+
+export function estDixMots(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const comments = control.value;
+      let mots : string[] = comments.split(' ')
+      if(!comments) {
+        return null;
+      }
+      const estValide = (mots.length >= 10);
+      return estValide ? null : { estDixMots: true}
+    };
+
+  }
+
+  export function nomDansCommentaire(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const nom = control.get('name');
+      const commentaire = control.get('comments');
+      if(!nom?.value || !commentaire?.value) {
+        return null;
+      }
+      const estValide = !commentaire.value.includes(nom.value);
+
+      if(!estValide) {
+        console.log("erreur");
+        commentaire.setErrors({ ...commentaire.errors, nomDansCommentaire: true });
+      }
+      
+      return estValide ? null : { nomDansCommentaire: true };
+    }
+  }
 
 
